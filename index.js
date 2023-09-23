910eb4fffbe8eb7fa43ee2b83d33077b0fc9dc38
HassanToyin <143343798+HassanToyin@users.noreply.github.com>
2023-09-23 22:38:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+let age = prompt("please enter your age")
+
+const citizen = prompt("are you a citizen: (yes/no)")
+
+if (age >= 18 && citizen === "yes"){
+    console.log("you are of age and a citizen so you are eligible to vote")
+}else if (age >= 18 && citizen === "no"){
+    console.log("you are of age and not a citizen so you are unable to vote")
+
+}else if(age < 18 && citizen === "yes"){
+    console.log("you are a citizen but not of age to vote")
+}else if(age < 18 && citizen === "no"){
+    console.log("you are not of age and not a citizen so you are unable to vote")
+
+}
+
